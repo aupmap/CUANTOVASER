ce2a25c554463c72bd06342d05cb14111ecb1769
aurora <padillaurora@gmail.com>
2021-02-23 13:59:00 -0600

    Logo menu nav

--- main.js
+++ main.js
@@ -8,13 +8,3 @@ window.addEventListener("scroll", function()
     * 0.7 + "px";
 }
 
-//Media queries
-/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
-    }
-  } 
